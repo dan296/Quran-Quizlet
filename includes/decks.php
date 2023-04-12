ee2fddbcc57d86a253f32341936118357d224b36
Daniyal <dan296@gmail.com>
2023-04-12 13:42:28 -0500

    editing folder structure

--- includes/decks.php
+++ includes/decks.php
@@ -1,21 +1,3 @@
-<div id="decks">
-  <?php include("./assets/logos-01.svg") ?>
-    <section class="decks">
-      <div class='setting-title'>Decks</div>
-      <div id="deck-collection">
-            You have no decks
-      </div>
-    </section>
-    <section class="decks">
-      <div class='setting-wrap'>
-            <button id="add-deck-btn">+ Add a Deck</button>
-        </div>
-    </section>
-    
-    <div id="version2">
-      <a href="https://github.com/dan296" target="_blank"><i class="fab fa-github"></i> <span>v1.0.0</span></a>
-    </div>
-</div>
 <div id="edit-deck" class="perf-center">
   <div class='exit cross crossrotate' style=''><div class="innercross"><div class="vert-cross"></div><div class="horiz-cross"></div></div></div>
   <input type="text" id="deck_name" placeholder="Deck Name"></input>
