cd2ae3c2ca2cc0a8733164f7580d860e9660af17
Daniyal <dan296@gmail.com>
2023-04-15 00:05:38 -0500

    editing folder structure

--- includes/juzs.php
+++ includes/juzs.php
@@ -1,4 +1,4 @@
-<div id="juz0" class="juzcont" style="display: block;"><div
+<div id="juz0" class="juzcont"><div
 class="juz-label"><span>1</span></div><div class="juz-label
 juz-mini-label"><span>1</span></div><div class="juz-label
 juz-mini-label"><span>2</span></div><div class="cross
@@ -8,7 +8,7 @@ class="vert-cross"></div></div></div><div class="juzname">Alīf-Lām-Mīm
 | آلم</div><div class="surahname">Al-Faatiha |
 ٱلْفَاتِحَةِ<span>Al-Baqara | البَقَرَةِ</span></div><div
 class="surah-name-trans">Ayah 1<span>Ayah 141</span></div></div><div
-id="juz1" class="juzcont" style="display: block;"><div
+id="juz1" class="juzcont"><div
 class="juz-label"><span>2</span></div><div class="juz-label
 juz-mini-label"><span>2</span></div><div class="juz-label
 juz-mini-label"><span>2</span></div><div class="cross
@@ -18,7 +18,7 @@ class="vert-cross"></div></div></div><div class="juzname">Sayaqūlu |
 سَيَقُولُ</div><div class="surahname">Al-Baqara |
 البَقَرَةِ<span>Al-Baqara | البَقَرَةِ</span></div><div
 class="surah-name-trans">Ayah 142<span>Ayah 252</span></div></div><div
-id="juz2" class="juzcont" style="display: block;"><div
+id="juz2" class="juzcont"><div
 class="juz-label"><span>3</span></div><div class="juz-label
 juz-mini-label"><span>2</span></div><div class="juz-label
 juz-mini-label"><span>3</span></div><div class="cross
@@ -28,7 +28,7 @@ class="vert-cross"></div></div></div><div class="juzname">Tilka
 'r-Rusulu | تِلْكَ ٱلْرُّسُلُ</div><div class="surahname">Al-Baqara |
 البَقَرَةِ<span>Aal-i-Imraan | آلِ عِمۡرَانَ</span></div><div
 class="surah-name-trans">Ayah 253<span>Ayah 92</span></div></div><div
-id="juz3" class="juzcont" style="display: block;"><div
+id="juz3" class="juzcont"><div
 class="juz-label"><span>4</span></div><div class="juz-label
 juz-mini-label"><span>3</span></div><div class="juz-label
 juz-mini-label"><span>4</span></div><div class="cross
@@ -38,7 +38,7 @@ class="vert-cross"></div></div></div><div class="juzname">Lan Tana Lu
 | لَنْ تَنَالُوْ الْبِرَّ</div><div class="surahname">Aal-i-Imraan |
 آلِ عِمۡرَانَ<span>An-Nisaa | النِّسَاءِ</span></div><div
 class="surah-name-trans">Ayah 93<span>Ayah 23</span></div></div><div
-id="juz4" class="juzcont" style="display: block;"><div
+id="juz4" class="juzcont"><div
 class="juz-label"><span>5</span></div><div class="juz-label
 juz-mini-label"><span>4</span></div><div class="juz-label
 juz-mini-label"><span>4</span></div><div class="cross
@@ -48,8 +48,7 @@ class="vert-cross"></div></div></div><div
 class="juzname">Wa’l-muḥṣanātu | وَٱلْمُحْصَنَاتُ</div><div
 class="surahname">An-Nisaa | النِّسَاءِ<span>An-Nisaa |
 النِّسَاءِ</span></div><div class="surah-name-trans">Ayah 24<span>Ayah
-147</span></div></div><div id="juz5" class="juzcont" style="display:
-block;"><div class="juz-label"><span>6</span></div><div
+147</span></div></div><div id="juz5" class="juzcont"><div class="juz-label"><span>6</span></div><div
 class="juz-label juz-mini-label"><span>4</span></div><div
 class="juz-label juz-mini-label"><span>5</span></div><div class="cross
 crossrotate"><div class="innercross"><div
@@ -69,7 +68,7 @@ class="vert-cross"></div></div></div><div class="juzname">Wa ’Idha
 Samiʿū | وَإِذَا سَمِعُوا</div><div class="surahname">Al-Maaida |
 المَائـِدَةِ<span>Al-An'aam | الأَنۡعَامِ</span></div><div
 class="surah-name-trans">Ayah 82<span>Ayah 110</span></div></div><div
-id="juz7" class="juzcont" style="display: block;"><div
+id="juz7" class="juzcont"><div
 class="juz-label"><span>8</span></div><div class="juz-label
 juz-mini-label"><span>6</span></div><div class="juz-label
 juz-mini-label"><span>7</span></div><div class="cross
@@ -79,7 +78,7 @@ class="vert-cross"></div></div></div><div class="juzname">Wa-law
 annanā | وَلَوْ أَنَّنَا</div><div class="surahname">Al-An'aam |
 الأَنۡعَامِ<span>Al-A'raaf | الأَعۡرَافِ</span></div><div
 class="surah-name-trans">Ayah 111<span>Ayah 87</span></div></div><div
-id="juz8" class="juzcont" style="display: block;"><div
+id="juz8" class="juzcont"><div
 class="juz-label"><span>9</span></div><div class="juz-label
 juz-mini-label"><span>7</span></div><div class="juz-label
 juz-mini-label"><span>8</span></div><div class="cross
@@ -89,7 +88,7 @@ class="vert-cross"></div></div></div><div class="juzname">Qāla
 ’l-mala’u | قَالَ ٱلْمَلَأُ</div><div class="surahname">Al-A'raaf |
 الأَعۡرَافِ<span>Al-Anfaal | الأَنفَالِ</span></div><div
 class="surah-name-trans">Ayah 88<span>Ayah 40</span></div></div><div
-id="juz9" class="juzcont" style="display: block;"><div
+id="juz9" class="juzcont"><div
 class="juz-label"><span>10</span></div><div class="juz-label
 juz-mini-label"><span>8</span></div><div class="juz-label
 juz-mini-label"><span>9</span></div><div class="cross
@@ -99,7 +98,7 @@ class="vert-cross"></div></div></div><div class="juzname">Wa-’aʿlamū |
 وَٱعْلَمُواْ</div><div class="surahname">Al-Anfaal |
 الأَنفَالِ<span>At-Tawba | التَّوۡبَةِ</span></div><div
 class="surah-name-trans">Ayah 41<span>Ayah 92</span></div></div><div
-id="juz10" class="juzcont" style="display: block;"><div
+id="juz10" class="juzcont"><div
 class="juz-label"><span>11</span></div><div class="juz-label
 juz-mini-label"><span>9</span></div><div class="juz-label
 juz-mini-label"><span>11</span></div><div class="cross
@@ -109,7 +108,7 @@ class="vert-cross"></div></div></div><div class="juzname">Yaʿtazerūn |
 يَعْتَذِرُونَ</div><div class="surahname">At-Tawba |
 التَّوۡبَةِ<span>Hud | هُودٍ</span></div><div
 class="surah-name-trans">Ayah 93<span>Ayah 5</span></div></div><div
-id="juz11" class="juzcont" style="display: block;"><div
+id="juz11" class="juzcont"><div
 class="juz-label"><span>12</span></div><div class="juz-label
 juz-mini-label"><span>11</span></div><div class="juz-label
 juz-mini-label"><span>12</span></div><div class="cross
@@ -119,7 +118,7 @@ class="vert-cross"></div></div></div><div class="juzname">Wa mā min
 dābbatin | وَمَا مِنْ دَآبَّةٍ</div><div class="surahname">Hud |
 هُودٍ<span>Yusuf | يُوسُفَ</span></div><div
 class="surah-name-trans">Ayah 6<span>Ayah 52</span></div></div><div
-id="juz12" class="juzcont" style="display: block;"><div
+id="juz12" class="juzcont"><div
 class="juz-label"><span>13</span></div><div class="juz-label
 juz-mini-label"><span>12</span></div><div class="juz-label
 juz-mini-label"><span>14</span></div><div class="cross
@@ -129,7 +128,7 @@ class="vert-cross"></div></div></div><div class="juzname">Wa mā
 ubarri’u | وَمَا أُبَرِّئُ</div><div class="surahname">Yusuf |
 يُوسُفَ<span>Ibrahim | إِبۡرَاهِيمَ</span></div><div
 class="surah-name-trans">Ayah 53<span>Ayah 52</span></div></div><div
-id="juz13" class="juzcont" style="display: block;"><div
+id="juz13" class="juzcont"><div
 class="juz-label"><span>14</span></div><div class="juz-label
 juz-mini-label"><span>15</span></div><div class="juz-label
 juz-mini-label"><span>16</span></div><div class="cross
@@ -139,8 +138,7 @@ class="vert-cross"></div></div></div><div
 class="juzname">Alīf-Lām-Rā’/Rubamā | رُبَمَا</div><div
 class="surahname">Al-Hijr | الحِجۡرِ<span>An-Nahl |
 النَّحۡلِ</span></div><div class="surah-name-trans">Ayah 1<span>Ayah
-128</span></div></div><div id="juz14" class="juzcont" style="display:
-block;"><div class="juz-label"><span>15</span></div><div
+128</span></div></div><div id="juz14" class="juzcont"><div class="juz-label"><span>15</span></div><div
 class="juz-label juz-mini-label"><span>17</span></div><div
 class="juz-label juz-mini-label"><span>18</span></div><div
 class="cross crossrotate"><div class="innercross"><div
@@ -149,7 +147,7 @@ class="vert-cross"></div></div></div><div class="juzname">Subḥāna
 ’lladhī | سُبْحَانَ ٱلَّذِى</div><div class="surahname">Al-Israa |
 الإِسۡرَاءِ<span>Al-Kahf | الكَهۡفِ</span></div><div
 class="surah-name-trans">Ayah 1<span>Ayah 74</span></div></div><div
-id="juz15" class="juzcont" style="display: block;"><div
+id="juz15" class="juzcont"><div
 class="juz-label"><span>16</span></div><div class="juz-label
 juz-mini-label"><span>18</span></div><div class="juz-label
 juz-mini-label"><span>20</span></div><div class="cross
@@ -159,7 +157,7 @@ class="vert-cross"></div></div></div><div class="juzname">Qāla ’alam |
 قَالَ أَلَمْ</div><div class="surahname">Al-Kahf |
 الكَهۡفِ<span>Taa-Haa | طه</span></div><div
 class="surah-name-trans">Ayah 75<span>Ayah 135</span></div></div><div
-id="juz16" class="juzcont" style="display: block;"><div
+id="juz16" class="juzcont"><div
 class="juz-label"><span>17</span></div><div class="juz-label
 juz-mini-label"><span>21</span></div><div class="juz-label
 juz-mini-label"><span>22</span></div><div class="cross
@@ -169,8 +167,7 @@ class="vert-cross"></div></div></div><div class="juzname">Iqtaraba
 li’n-nāsi | ٱقْتَرَبَ لِلْنَّاسِ</div><div
 class="surahname">Al-Anbiyaa | الأَنبِيَاءِ<span>Al-Hajj |
 الحَجِّ</span></div><div class="surah-name-trans">Ayah 1<span>Ayah
-78</span></div></div><div id="juz17" class="juzcont" style="display:
-block;"><div class="juz-label"><span>18</span></div><div
+78</span></div></div><div id="juz17" class="juzcont"><div class="juz-label"><span>18</span></div><div
 class="juz-label juz-mini-label"><span>23</span></div><div
 class="juz-label juz-mini-label"><span>25</span></div><div
 class="cross crossrotate"><div class="innercross"><div
@@ -179,7 +176,7 @@ class="vert-cross"></div></div></div><div class="juzname">Qad ’aflaḥa
 | قَدْ أَفْلَحَ</div><div class="surahname">Al-Muminoon |
 المُؤۡمِنُونَ<span>Al-Furqaan | الفُرۡقَانِ</span></div><div
 class="surah-name-trans">Ayah 1<span>Ayah 20</span></div></div><div
-id="juz18" class="juzcont" style="display: block;"><div
+id="juz18" class="juzcont"><div
 class="juz-label"><span>19</span></div><div class="juz-label
 juz-mini-label"><span>25</span></div><div class="juz-label
 juz-mini-label"><span>27</span></div><div class="cross
@@ -189,7 +186,7 @@ class="vert-cross"></div></div></div><div class="juzname">Wa-qāla
 ’lladhīna | وَقَالَ ٱلَّذِينَ</div><div class="surahname">Al-Furqaan |
 الفُرۡقَانِ<span>An-Naml | النَّمۡلِ</span></div><div
 class="surah-name-trans">Ayah 21<span>Ayah 55</span></div></div><div
-id="juz19" class="juzcont" style="display: block;"><div
+id="juz19" class="juzcont"><div
 class="juz-label"><span>20</span></div><div class="juz-label
 juz-mini-label"><span>27</span></div><div class="juz-label
 juz-mini-label"><span>29</span></div><div class="cross
@@ -199,7 +196,7 @@ class="vert-cross"></div></div></div><div class="juzname">’A’man
 Khalaqa | أَمَّنْ خَلَقَ</div><div class="surahname">An-Naml |
 النَّمۡلِ<span>Al-Ankaboot | العَنكَبُوتِ</span></div><div
 class="surah-name-trans">Ayah 56<span>Ayah 45</span></div></div><div
-id="juz20" class="juzcont" style="display: block;"><div
+id="juz20" class="juzcont"><div
 class="juz-label"><span>21</span></div><div class="juz-label
 juz-mini-label"><span>29</span></div><div class="juz-label
 juz-mini-label"><span>33</span></div><div class="cross
@@ -209,7 +206,7 @@ class="vert-cross"></div></div></div><div class="juzname">Otlu ma
 oohiya | أُتْلُ مَاأُوْحِیَ </div><div class="surahname">Al-Ankaboot |
 العَنكَبُوتِ<span>Al-Ahzaab | الأَحۡزَابِ</span></div><div
 class="surah-name-trans">Ayah 46<span>Ayah 30</span></div></div><div
-id="juz21" class="juzcont" style="display: block;"><div
+id="juz21" class="juzcont"><div
 class="juz-label"><span>22</span></div><div class="juz-label
 juz-mini-label"><span>33</span></div><div class="juz-label
 juz-mini-label"><span>36</span></div><div class="cross
@@ -219,7 +216,7 @@ class="vert-cross"></div></div></div><div class="juzname">Wa-man
 yaqnut | وَمَنْ يَّقْنُتْ</div><div class="surahname">Al-Ahzaab |
 الأَحۡزَابِ<span>Yaseen | يسٓ</span></div><div
 class="surah-name-trans">Ayah 31<span>Ayah 27</span></div></div><div
-id="juz22" class="juzcont" style="display: block;"><div
+id="juz22" class="juzcont"><div
 class="juz-label"><span>23</span></div><div class="juz-label
 juz-mini-label"><span>36</span></div><div class="juz-label
 juz-mini-label"><span>39</span></div><div class="cross
@@ -228,8 +225,7 @@ class="horiz-cross"></div><div
 class="vert-cross"></div></div></div><div class="juzname">Wa-Mali |
 وَمَآ لي</div><div class="surahname">Yaseen | يسٓ<span>Az-Zumar |
 الزُّمَرِ</span></div><div class="surah-name-trans">Ayah 28<span>Ayah
-31</span></div></div><div id="juz23" class="juzcont" style="display:
-block;"><div class="juz-label"><span>24</span></div><div
+31</span></div></div><div id="juz23" class="juzcont"><div class="juz-label"><span>24</span></div><div
 class="juz-label juz-mini-label"><span>39</span></div><div
 class="juz-label juz-mini-label"><span>41</span></div><div
 class="cross crossrotate"><div class="innercross"><div
@@ -238,7 +234,7 @@ class="vert-cross"></div></div></div><div class="juzname">Fa-man
 ’aẓlamu | فَمَنْ أَظْلَمُ</div><div class="surahname">Az-Zumar |
 الزُّمَرِ<span>Fussilat | فُصِّلَتۡ</span></div><div
 class="surah-name-trans">Ayah 32<span>Ayah 46</span></div></div><div
-id="juz24" class="juzcont" style="display: block;"><div
+id="juz24" class="juzcont"><div
 class="juz-label"><span>25</span></div><div class="juz-label
 juz-mini-label"><span>41</span></div><div class="juz-label
 juz-mini-label"><span>45</span></div><div class="cross
@@ -248,7 +244,7 @@ class="vert-cross"></div></div></div><div class="juzname">Ilayhi
 yuraddu | إِلَيْهِ يُرَدُّ</div><div class="surahname">Fussilat |
 فُصِّلَتۡ<span>Al-Jaathiya | الجَاثِيَةِ</span></div><div
 class="surah-name-trans">Ayah 47<span>Ayah 37</span></div></div><div
-id="juz25" class="juzcont" style="display: block;"><div
+id="juz25" class="juzcont"><div
 class="juz-label"><span>26</span></div><div class="juz-label
 juz-mini-label"><span>46</span></div><div class="juz-label
 juz-mini-label"><span>51</span></div><div class="cross
@@ -258,7 +254,7 @@ class="vert-cross"></div></div></div><div class="juzname">Ḥā’ Mīm |
 حم</div><div class="surahname">Al-Ahqaf |
 الأَحۡقَافِ<span>Adh-Dhaariyat | الذَّارِيَاتِ</span></div><div
 class="surah-name-trans">Ayah 1<span>Ayah 30</span></div></div><div
-id="juz26" class="juzcont" style="display: block;"><div
+id="juz26" class="juzcont"><div
 class="juz-label"><span>27</span></div><div class="juz-label
 juz-mini-label"><span>51</span></div><div class="juz-label
 juz-mini-label"><span>57</span></div><div class="cross
@@ -268,8 +264,7 @@ class="vert-cross"></div></div></div><div class="juzname">Qāla fa-mā
 khaṭbukum | قَالَ فَمَا خَطْبُكُم</div><div
 class="surahname">Adh-Dhaariyat | الذَّارِيَاتِ<span>Al-Hadid |
 الحَدِيدِ</span></div><div class="surah-name-trans">Ayah 31<span>Ayah
-29</span></div></div><div id="juz27" class="juzcont" style="display:
-block;"><div class="juz-label"><span>28</span></div><div
+29</span></div></div><div id="juz27" class="juzcont"><div class="juz-label"><span>28</span></div><div
 class="juz-label juz-mini-label"><span>58</span></div><div
 class="juz-label juz-mini-label"><span>66</span></div><div
 class="cross crossrotate"><div class="innercross"><div
@@ -278,7 +273,7 @@ class="vert-cross"></div></div></div><div class="juzname">Qad samiʿa
 ’llāhu | قَدْ سَمِعَ ٱللهُ</div><div class="surahname">Al-Mujaadila |
 المُجَادلَةِ<span>At-Tahrim | التَّحۡرِيمِ</span></div><div
 class="surah-name-trans">Ayah 1<span>Ayah 12</span></div></div><div
-id="juz28" class="juzcont" style="display: block;"><div
+id="juz28" class="juzcont"><div
 class="juz-label"><span>29</span></div><div class="juz-label
 juz-mini-label"><span>67</span></div><div class="juz-label
 juz-mini-label"><span>77</span></div><div class="cross
@@ -288,7 +283,7 @@ class="vert-cross"></div></div></div><div class="juzname">Tabāraka
 ’lladhī | تَبَارَكَ ٱلَّذِى</div><div class="surahname">Al-Mulk |
 المُلۡكِ<span>Al-Mursalaat | المُرۡسَلَاتِ</span></div><div
 class="surah-name-trans">Ayah 1<span>Ayah 50</span></div></div><div
-id="juz29" class="juzcont" style="display: block;"><div
+id="juz29" class="juzcont"><div
 class="juz-label"><span>30</span></div><div class="juz-label
 juz-mini-label"><span>78</span></div><div class="juz-label
 juz-mini-label"><span>114</span></div><div class="cross
@@ -297,5 +292,4 @@ class="horiz-cross"></div><div
 class="vert-cross"></div></div></div><div class="juzname">'Amma |
 عَمَّ</div><div class="surahname">An-Naba | النَّبَإِ<span>An-Naas |
 النَّاسِ</span></div><div class="surah-name-trans">Ayah 1<span>Ayah
-6</span></div></div><div id="juz30" class="juzcont" style="display:
-block;"></div>
+6</span></div></div><div id="juz30" class="juzcont"></div>
