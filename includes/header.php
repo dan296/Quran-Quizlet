aca1179673abf0f636408a4450ee05640e11ee11
Daniyal <dan296@gmail.com>
2023-04-16 00:54:13 -0500

    editing folder structure

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<div id="header-wrap">
+  <div id="header">
+    <?php include("./assets/logos-01.svg") ?>
+    <div id="search"><i class="fa fa-search"></i><input type="text" placeholder="Ex: Mankind" onkeyup="filterSearch()"></div>
+    <div id="surahOrJuz">
+      <div class="sur-juz-selected">Surah</div>
+      <div>Juz</div>
+    </div>
+  </div>
+</div>
+<i id="sorter" title="sort" class="fa fa-chevron-down guide" aria-hidden="true"></i>
