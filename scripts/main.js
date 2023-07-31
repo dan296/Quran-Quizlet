23e3ce78d52313daff41e25075f7d3ae4c9b572c
Daniyal <dan296@gmail.com>
2023-07-31 08:18:42 -0500

    functional update

--- scripts/main.js
+++ scripts/main.js
@@ -413,7 +413,7 @@ $("#edit-deck .exit").click(function(){
 })
 
 $("#save-deck").click(function(){
-  if(decks.filter((e, idx) => (e.name === $("#deck_name").val() && idx !== editing_deck_id)).length > 0 || $("#deck_name").val() == ""){
+  if(editing_deck_id == -1 && decks.filter((e, idx) => (e.name === $("#deck_name").val() && idx !== editing_deck_id)).length > 0 || $("#deck_name").val() == ""){
     alert("Please enter a unique Deck Name");
   }else{
 
