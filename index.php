e7a6fc4862402a989e98e787e498860efb9f07d3
Daniyal <dan296@gmail.com>
2023-08-02 06:27:57 -0500

    updating login

--- index.php
+++ index.php
@@ -29,24 +29,24 @@ include("backend/db_connection.php");
 
 if(isset($_COOKIE["member_login"]) && $_COOKIE["member_login"] !== ""){
   $user = $_COOKIE["member_login"];
-  $sql = "SELECT `email`, `user_name` FROM `users` WHERE `user_name`='$user' OR `email`='$user'";
+  $sql = "SELECT * FROM `users` WHERE `user_name`='$user' OR `email`='$user'";
   $result = $conn->query($sql);
   $results = $result -> fetch_array(MYSQLI_ASSOC);
-  //$results["decks"] = removeFirstAndLastChar($results["decks"], 'n');
-  //$results["decks"] = str_replace('"', "\'", $results["decks"]);
   ?>
   cookieSet = true;
   $('#rem-check input').click();
   thisuser = '<?php echo $_COOKIE["member_login"]; ?>';
   $('#signin input[type=text]').prop('value',thisuser);
-  let this_data = '<?php echo json_encode($results); ?>';
+  let this_data = <?php echo json_encode($results); ?>;
   let this_user_data = JSON.parse(this_data);
   $('#setting-email').html(this_user_data.email);
   $('#setting-email').attr('title', this_user_data.email);
   $('#setting-user').html(this_user_data.user_name);
   $('#signin-out-btn').html("Sign out");
   $('.setting-profile').show();
-  $('#loadingpage').hide();
+  showMain();
+  decks = this_user_data.decks !== "" ? JSON.parse(this_user_data.decks) : [];
+  updateDecks();
 <? } ?>  
 
 </script>
