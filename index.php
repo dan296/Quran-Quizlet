39bc54a5b274e8f47dd4d184cc54aa7510830ab1
Daniyal <dan296@gmail.com>
2023-08-01 20:56:22 -0500

    functional update

--- index.php
+++ index.php
@@ -14,18 +14,31 @@
 <script src="scripts/main.js?v=<?php echo rand()?>"></script>
 <script>
 <?php
+function removeFirstAndLastChar($inputString, $charToRemove) {
+    $firstIndex = strpos($inputString, $charToRemove);
+    $lastIndex = strrpos($inputString, $charToRemove);
+
+    if ($firstIndex !== false && $lastIndex !== false && $firstIndex !== $lastIndex) {
+        return substr_replace($inputString, '', $lastIndex, 1);
+    }
+
+    return $inputString;
+}
+
 include("backend/db_connection.php");
+
 if(isset($_COOKIE["member_login"]) && $_COOKIE["member_login"] !== ""){
   $user = $_COOKIE["member_login"];
   $sql = "SELECT * FROM `users` WHERE `user_name`='$user' OR `email`='$user'";
   $result = $conn->query($sql);
   $results = $result -> fetch_array(MYSQLI_ASSOC);
+  $results["decks"] = removeFirstAndLastChar($results["decks"], '"');
   ?>
   cookieSet = true;
   $('#rem-check input').click();
   thisuser = '<?php echo $_COOKIE["member_login"]; ?>';
   $('#signin input[type=text]').prop('value',thisuser);
-  let this_data = '<?php echo json_encode($results["decks"]); ?>';
+  let this_data = '<?php echo json_encode($results); ?>';
   let this_user_data = JSON.parse(this_data);
   $('#setting-email').html(this_user_data.email);
   $('#setting-email').attr('title', this_user_data.email);
@@ -34,6 +47,7 @@ if(isset($_COOKIE["member_login"]) && $_COOKIE["member_login"] !== ""){
   $('.setting-profile').show();
   $('#loadingpage').hide();
 <? } ?>  
+
 </script>
 </body>
 </html>
